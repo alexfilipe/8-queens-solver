b07f22855545015de550298a1144a5a2fb98fdf3
alexfilipe <alex@afcbs.me>
2020-07-27 02:30:25 -0400

    add readme and report generator

--- report.py
+++ report.py
@@ -33,14 +33,14 @@ def generate_test_cases(n=8, size=10, filepath="output/test.txt"):
 if __name__ == '__main__':
     # generate_test_cases(n=8, size=10, filepath="test/cases.txt")
 
-    # successful, size = random_test_solver(HillClimbingSolver, n=8, size=5000)
-    # percent = (successful / size) * 100
+    successful, size = random_test_solver(HillClimbingSolver, n=8, size=500)
+    percent = (successful / size) * 100
 
-    # print("Hill Climbing results: {0} successful out of {1} ({2:.2f}%)"
-    #       .format(successful, size, percent))
+    print("Hill Climbing results: {0} successful out of {1} ({2:.2f}%)"
+          .format(successful, size, percent))
 
     successful, size = random_test_solver(SimulatedAnnealingSolver, n=8,
-                                          size=100)
+                                          size=500)
     percent = (successful / size) * 100
 
     print("Simulated Annealing results: {0} successful out of {1} ({2:.2f}%)"
